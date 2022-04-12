595925aa716da0353915b974cbe0c865b70652e8
Johan <johanedfb@gmail.com>
2022-04-11 20:42:22 -0600

    ignore

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const apiTMDB = async() =>{
         const respuesta = await fetch(`
         https://api.themoviedb.org/3/movie/550?api_key=9c9fa07fa60b593274dda242137b4857&language=es-MX
         `);
-        
+
     }
 }

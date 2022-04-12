6da774498ebba6d1587965594087792b3198eb37
Johan <johanedfb@gmail.com>
2022-04-11 20:35:27 -0600

    Codigo Base

---
+++ index.js
@@ -0,0 +1,8 @@
+const apiTMDB = async() =>{
+    try{
+        const respuesta = await fetch(`
+        https://api.themoviedb.org/3/movie/550?api_key=9c9fa07fa60b593274dda242137b4857&language=es-MX
+        `);
+        
+    }
+}
